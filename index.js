ce3d316e8789741a2476c5d7aeaddbde4c81aa17
Sofian Hadiwijaya <sofian@warungpintar.co>
2022-07-20 10:17:16 +0700

    Metamask Connect

---
+++ index.js
@@ -0,0 +1,21 @@
+const { web3 } = require("web3");
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    document.getElementById("connectButton").innerHTML = "Connected";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  } else {
+    document.getElementById("connectButton").innerHTML =
+      "Please install MetaMask";
+  }
+}
+
+module.exports = {
+    connect,
+};
